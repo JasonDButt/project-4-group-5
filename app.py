a1c9149c6a6467972468468292b5209b90dea318
MZt92-ui <76995395+MZt92-ui@users.noreply.github.com>
2022-10-11 18:39:46 +1100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return render_template("index.html")
 
 # ==============================================
-# To read the input file path
+# To read the initial file path
 @app.route("/read_puzzle", methods=["POST","GET"])
 def puzzle():
     if request.method == "POST":
@@ -17,6 +17,20 @@ def puzzle():
         # data = export_result(input_path)
     return render_template("modify.html", test=input_path)
 
+# # ==============================================
+# # To read user's modification
+# @app.route("/modify_puzzle", methods=["POST","GET"])
+# def solve():
+#     # ==============================================
+#     # if the input is none, use the output for Solve Soduku & get list for rendering modify page
+#     if request.method == "POST":
+#         input_path =request.form.get("url_path")
+#         # data = export_result(input_path)
+#     return render_template("modify.html", test=input_path)
+
+#     # ==============================================
+#     # if the input with values, modify the input and serve for Solve Soduku & get list for rendering modify page
+#     return render_template("modify.html", test=input_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
