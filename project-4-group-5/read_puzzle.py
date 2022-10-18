2b744ac5904baf12282f83ef998ea63e9a2bc98b
Jason Butt <jasonbutt13@gmail.com>
2022-10-18 18:24:04 +1100

    try changing other paths1

--- project-4-group-5/read_puzzle.py
+++ project-4-group-5/read_puzzle.py
@@ -96,7 +96,7 @@ def display_numbers_on_board(board,puzzle):
 # Determine step size for x and y directions (cell size)
 # Create board and initialise values as 0
 def export_result(img_path):
-    model = load_model('model/model_mnist/')
+    model = load_model('project-4-group-5/model/model_mnist/')
     # New model
     # model = load_model('model/model/')
     img_shape = [28,28]
